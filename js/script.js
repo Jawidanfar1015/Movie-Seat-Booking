66e402675caca674d7ebfae25cc1f613139adf88
Jawid Noori <Hajikaka4030@gmail.com>
2022-05-23 23:07:02 -0700

    data saved to localStorage

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 const container = document.querySelector('.container');
-const seat = document.querySelectorAll('.row .seat:not(.occupied)');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value;
 
+function setMovieData(movieIndex, moviePrice){
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 function updateSelectedCount(){
-    const selectedSeat = document.querySelectorAll('.row .seat.selected');
-    const selectedSeatCount = selectedSeat.length;
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+    
+    
+    const selectedSeatCount = selectedSeats.length;
    
     count.innerText = selectedSeatCount;
     total.innerText = selectedSeatCount * ticketPrice;
@@ -16,6 +26,7 @@ function updateSelectedCount(){
 
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 })
 

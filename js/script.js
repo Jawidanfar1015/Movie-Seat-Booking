87d4ca92360efb165066d34dc10f297ed6d28ab9
Jawid Noori <Hajikaka4030@gmail.com>
2022-05-23 22:23:46 -0700

    Event listener added to the seats

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const container = document.querySelector('.container');
+const seat = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+let ticketPrice = +movieSelect.value;
+
+function updateSelectedCount(){
+    const selectedSeat = document.querySelectorAll('.row .seat.selected');
+    const selectedSeatCount = selectedSeat.length;
+   
+    count.innerText = selectedSeatCount;
+    total.innerText = selectedSeatCount * ticketPrice;
+}
+
+movieSelect.addEventListener('change', e => {
+    ticketPrice = +e.target.value;
+    updateSelectedCount();
+})
+
+container.addEventListener('click', e => {
+    if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
+        e.target.classList.toggle('selected');
+        updateSelectedCount();
+    }
+})
